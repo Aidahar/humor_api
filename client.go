1b735c81aa9df4688441c68d2137eeb3a9da4045
Aidahar <zagirik@gmail.com>
2021-11-02 13:28:42 +0300

    change functionl

--- client.go
+++ client.go
@@ -1,51 +1,51 @@
 package humor_api
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "encoding/json"
-    "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 const (
-    BaseURLV1 = "https://api.humorapi.com/jokes/random"
+	BaseURLV1 = "https://api.humorapi.com/jokes/random"
 )
 
 type Client struct {
-    BaseURL string
-    apiKey string
-    HTTPClient *http.Client
+	BaseURL    string
+	apiKey     string
+	HTTPClient *http.Client
 }
 
-func NewClient(apiKey string) *Client {
-    return &Client{
-        BaseURL: BaseURLV1,
-        apiKey: apiKey,
-        HTTPClient: &http.Client{
-            Timeout: time.Minute,
-        },
-    }
+func NewClient(apiKey string) (*Client, error) {
+	return &Client{
+		BaseURL: BaseURLV1,
+		apiKey:  apiKey,
+		HTTPClient: &http.Client{
+			Timeout: time.Minute,
+		},
+	}, nil
 }
 
-func (c Client) GetJoke(random string) (joke, error) {
-    url := fmt.Sprintf("%s?api-key=%s&", c.BaseURL, c.apiKey)
-    resp, err := c.HTTPClient.Get(url)
-    if err != nil {
-        return joke{}, err
-    }
+func (c Client) GetJoke() (Joke, error) {
+	url := fmt.Sprintf("%s?api-key=%s", c.BaseURL, c.apiKey)
+	resp, err := c.HTTPClient.Get(url)
+	if err != nil {
+		return Joke{}, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    if err != nil {
-        return joke{}, err
-    }
+	if err != nil {
+		return Joke{}, err
+	}
 
-    var jokeData joke
-    if err := json.Unmarshal(body, &jokeData); err != nil {
-        return joke{}, err
-    }
-    return jokeData, nil
+	var jokeData Joke
+	if err := json.Unmarshal(body, &jokeData); err != nil {
+		return Joke{}, err
+	}
+	return jokeData, nil
 }
--- joke.go
+++ joke.go
@@ -1,6 +1,12 @@
 package humor_api
 
+import "fmt"
+
 type Joke struct {
-    id string `json:"id"`
-    joke string `json:"joke"`
+	id   string `json:"id"`
+	joke string `json:"joke"`
+}
+
+func (j Joke) Info() string {
+	return fmt.Sprintf("[Id] %s | [Joke] %s", j.id, j.joke)
 }
